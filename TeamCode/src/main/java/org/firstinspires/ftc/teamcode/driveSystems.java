4456365bf92f56ea0846f54ef742ca13ab5a9ae3
rohankulkz <60551302+rohankulkz@users.noreply.github.com>
2021-08-16 21:42:47 -0700

    Added angle implementation for continuous drive and a method for turn cycle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveSystems.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveSystems.java
@@ -6,7 +6,7 @@ public class driveSystems {
     public driveSystems(){
     }
 
-    public static double[] continousDrive(double x_value, double y_value){
+    public static double[] continousDriveWithCoords(double x_value, double y_value){
         /*
         * Method takes joystick input and returns 4 motor powers from -1 to 1 to create a continuous mechanum drive
         * Commit movement allows the system to either commit the movement and return data, or just return data -----DEPRECATED------
@@ -49,6 +49,59 @@ public class driveSystems {
              {
               motor_powers[i] = mag*(d[start][i] + ((theta - t[start])/(t[end] - t[start]))*(d[end][i] - d[start][i]));
              }
+
+
+        return motor_powers;
+    }
+    public static double[] turnDrive(double x_turn_value){
+     double[] motorValues = {x_turn_value,x_turn_value,x_turn_value,x_turn_value};
+
+     return motorValues;
+    }
+
+    public static double[] continousDriveWithAngle(double theta, double speed){
+        /*
+         * Method takes an angle and returns 4 motor powers from -1 to 1 to create a continuous mechanum drive
+         * Commit movement allows the system to either commit the movement and return data, or just return data -----DEPRECATED------
+         * This method returns 4 motor powers for an all wheel drive
+         * NOTE: Motor value may need to be made negative or positive and their order manipulated to fit your drive train
+         * NOTE: This method is optimal for autonomous code as it takes an angle as an input
+         *
+         *
+         *
+         * */
+
+
+        double[] motor_powers = {0,0,0,0};  // {fl, fr, rl, rr}
+
+        double t[] = {  0, 0.25*Math.PI, 0.5*Math.PI, 0.75*Math.PI, Math.PI,
+                1.25*Math.PI, 1.5*Math.PI, 1.75*Math.PI, 2.0*Math.PI};
+        // d = direction, in this order = fl -> fr -> rl -> rr
+        double d[][] = {    {-1, -1, 1, 1}, // E
+                {-1, 0, 0, 1}, // NE
+                {-1, 1, -1, 1}, // N
+                {0, 1, -1, 0}, // NW
+                {1, 1, -1, -1}, // W
+                {1, 0, 0, -1}, // SW
+                {1, -1, 1, -1}, // S
+                {0, -1, 1, 0}, // SE
+                {-1, -1, 1, 1}}; // E
+        int fl = 0, fr = 1, rl = 2, rr = 3;
+
+        int direction = 0;
+
+
+        if (theta < 0.0)
+        {
+            theta = theta + 2*Math.PI;
+        }
+
+        int start = ((int) (theta/(0.25*Math.PI))) % 8;
+        int end = start + 1;
+        for (int i = 0; i < 4; i++)
+        {
+            motor_powers[i] = speed*(d[start][i] + ((theta - t[start])/(t[end] - t[start]))*(d[end][i] - d[start][i]));
+        }
 
 
         return motor_powers;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -6,7 +6,7 @@ public class test {
     public static void main(String[] args) {
 
 
-        double[] values = driveSystems.continousDrive(-0.4,0.6);
+        double[] values = driveSystems.continousDriveWithCoords(-0.4,0.6);
 
         for (int i = 0; i < 4; i++) {
             System.out.println("Section "+i+": "+values[i]);
